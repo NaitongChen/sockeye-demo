c665cb080aa36f7074161bf9d6d8901297226a69
NaitongChen <chennaitong218@hotmail.com>
2023-06-07 09:42:31 -0700

    added job script

--- init-config.sh
+++ init-config.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-cd /scratch/st-tdjc-1/naitong-demo
 julia -e 'using Pkg; Pkg.Registry.rm("General")'
-JULIA_PKG_USE_CLI_GIT=true julia /scratch/st-tdjc-1/naitong-demo/sockeye-demo/julia-setup-env.jl
+JULIA_PKG_USE_CLI_GIT=true julia julia-setup-env.jl
 julia --project=jlenv -e 'using Pkg; Pkg.instantiate(); Pkg.resolve(); Pkg.precompile()'
